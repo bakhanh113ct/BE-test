4c694074b9985991ed08f403021927dee412b033
bakhanh113ct <84719735+bakhanh113ct@users.noreply.github.com>
2023-10-12 23:41:30 +0700

    test redis

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -1,14 +1,14 @@
-// import { createClient } from 'redis';
-// import { config } from 'dotenv';
-// // import { Log, getProcessId } from '../utils/function';
-// // import moment from 'moment';
+import { createClient } from 'redis';
+import { config } from 'dotenv';
+// import { Log, getProcessId } from '../utils/function';
+// import moment from 'moment';
 
-// config();
+config();
 
-// export const client = createClient({
-//   url: process.env.REDIS_URL!,
-// });
+export const client = createClient({
+  url: process.env.REDIS_URL!,
+});
 
-// (async () => {
-//   await client.connect();
-// })();
+(async () => {
+  await client.connect();
+})();
